6f16c595cdeb591acac446e4c5ea474b4fad64eb
dreamerkyle <kyle_zh@outlook.com>
2023-08-06 20:08:35 +0800

    initial project version

---
+++ test.py
@@ -0,0 +1,13 @@
+import numpy as np 
+import pandas as pd
+import os
+
+def import_excel(file_path):
+    os.chdir("/Users/kylezh/Desktop/myzone")
+    df = pd.read_excel(file_path)
+    print(df)
+
+if __name__=="__main__":
+    
+    file_path = "testingdata.xlsx"
+    import_excel(file_path)
