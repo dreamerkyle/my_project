8ecb2486389df3971673067dc7df05b6ac7f7492
dreamerkyle <kyle_zh@outlook.com>
2023-08-07 20:16:22 +0800

    das

--- test.py
+++ test.py
@@ -1,17 +1,19 @@
-import numpy as np 
+import numpy as np
 import pandas as pd
 import os
+
 
 def import_excel(file_path):
     os.chdir("/Users/kylezh/Desktop/myzone")
     df = pd.read_excel(file_path)
     print(df)
 
-if __name__=="__main__":
-    
+
+if __name__ == "__main__":
     file_path = "testingdata.xlsx"
     import_excel(file_path)
     a = 1
     b = 2
     d = 4
+    e = a-2-3
     c = 3
